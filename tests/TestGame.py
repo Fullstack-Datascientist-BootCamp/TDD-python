ecc9e891a119b87368dea1d766a0b0f4774d9106
liangshinegood <liangshinegood@gmail.com>
2018-01-14 20:57:33 +0800

    WIP 程瑞

--- tests/TestGame.py
+++ tests/TestGame.py
@@ -1,18 +1,42 @@
 import unittest
 from bowling import Game
 
-class TestGame(unittest.TestCase):
-    g = Game.Game()
+class TestGame(
+    
     def test_TwoThrowsNoMark(self):
-        self.g.add(5)
-        self.g.add(4)
-        self.assertEqual(9, self.g.score())
+        g = Game.Game()
+        g.add(5)
+        g.add(4)
+        self.assertEqual(9, g.score())
+        self.assertEqual(2,g.getCurrentFrame())
 
     def test_FourThrowsNoMark(self):
-        self.g.add(5)
-        self.g.add(4)
-        self.g.add(7)
-        self.g.add(2)
-        self.assertEqual(18, self.g.score())
-        self.assertEqual(9, self.g.scoreForFrame(1))
-        self.assertEqual(18, self.g.scoreForFrame(2))
+        g = Game.Game()
+        g.add(5)
+        g.add(4)
+        g.add(7)
+        g.add(2)
+        self.assertEqual(18, g.score())
+        self.assertEqual(9, g.scoreForFrame(1))
+        self.assertEqual(18, g.scoreForFrame(2))
+        self.assertEqual(3,g.getCurrentFrame())
+    def test_SimpleSpare(self):
+        g = Game.Game()
+        g.add(3)
+        g.add(7)
+        g.add(3)
+        self.assertEqual(13,g.scoreForFrame(1))
+    def test_SimpleAfterSparre(self):
+        g = Game.Game()
+        g.add(3)
+        g.add(7)
+        g.add(3)
+        g.add(2)
+#        self.assertEqual(18,g.score())
+        self.assertEqual(18,g.scoreForFrame(2))
+        self.assertEqual(13,g.scoreForFrame(1))
+    def test_oneThrows(self):
+        g = Game.Game()
+        g.add(5)
+        self.assertEqual(5,g.score())
+        self.assertEqual(1,g.getCurrentFrame())
