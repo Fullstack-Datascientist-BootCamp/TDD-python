397bef0e49b70819f4180d8ec004b62ea3de3270
liangshinegood <liangshinegood@gmail.com>
2018-01-14 22:30:24 +0800

    finish spare and part of strike

--- bowling/Game.py
+++ bowling/Game.py
@@ -6,36 +6,43 @@ class Game(object):
     itsCurrentFrame = 1
     firstThrow = True
     def score(self):
-        return self.itsScore 
+        return self.scoreForFrame(self.getCurrentFrame() - 1)
+    
     def add(self, pins):
         self.itsThrows.insert(self.itsCurrentThrow, pins) 
         self.itsCurrentThrow += 1
         self.itsScore += pins
-        self.adjustCurrentFrame()
+        self.adjustCurrentFrame(pins)
 
-    def adjustCurrentFrame(self):
-                
+    def adjustCurrentFrame(self,pins):
         if(self.firstThrow):
-            self.firstThrow = False
-            
+            if(pins == 10):
+                self.itsCurrentFrame += 1
+            else:
+                self.firstThrow = False
         else:
             self.firstThrow = True
             self.itsCurrentFrame += 1
+    
     def scoreForFrame(self, theFrame):
         score = 0
         ball = 0
         for currentFrame in range(theFrame):
-            
             firstThrow = self.itsThrows[ball] 
             secondThrow = self.itsThrows[ball + 1]
             frameScore = firstThrow + secondThrow
-            if(frameScore == 10):
-                score += frameScore + self.itsThrows[ball + 2]
-             
+            if(firstThrow == 10):
+                score += 10 + self.itsThrows[ball + 1] + self.itsThrows[ball +2]
+                ball += 1
             else:
-                score += frameScore
-            ball +=2
+                if(frameScore == 10):
+                    score += frameScore + self.itsThrows[ball + 2]
+                    ball +=2
+                else:
+                    score += frameScore
+                    ball +=2
         return score
+    
     def getCurrentFrame(self):
 #        return 1 + flour((self.itsCurrentThrow - 1)/2)
        # return ceil(self.itsCurrentThrow/2)
--- tests/TestGame.py
+++ tests/TestGame.py
@@ -1,7 +1,7 @@
 import unittest
 from bowling import Game
 
-class TestGame(
+class TestGame(unittest.TestCase):
     
     def test_TwoThrowsNoMark(self):
         g = Game.Game()
@@ -20,23 +20,37 @@ class TestGame(
         self.assertEqual(9, g.scoreForFrame(1))
         self.assertEqual(18, g.scoreForFrame(2))
         self.assertEqual(3,g.getCurrentFrame())
+    
     def test_SimpleSpare(self):
         g = Game.Game()
         g.add(3)
         g.add(7)
         g.add(3)
         self.assertEqual(13,g.scoreForFrame(1))
+        self.assertEqual(2,g.getCurrentFrame())
+    
     def test_SimpleAfterSparre(self):
         g = Game.Game()
         g.add(3)
         g.add(7)
         g.add(3)
         g.add(2)
-#        self.assertEqual(18,g.score())
+        self.assertEqual(18,g.score())
         self.assertEqual(18,g.scoreForFrame(2))
         self.assertEqual(13,g.scoreForFrame(1))
-    def test_oneThrows(self):
+        self.assertEqual(3,g.getCurrentFrame())
+
+    def test_test(self):
         g = Game.Game()
-        g.add(5)
-        self.assertEqual(5,g.score())
-        self.assertEqual(1,g.getCurrentFrame())
+        g.add(10)
+        g.add(3)
+        g.add(6)
+        self.assertEqual(19,g.scoreForFrame(1))
+        self.assertEqual(28,g.score())
+        self.assertEqual(3,g.getCurrentFrame())
+
+
+
+
+
+
